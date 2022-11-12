8d8647c78fde957042401df032e05c873bb3c5ee
Himal Puri <purihimal9@gmail.com>
2022-11-12 20:04:56 +0100

    multi-color-line chart

--- practice/scatter/index.js
+++ practice/scatter/index.js
@@ -1,4 +1,3 @@
-
 const titleText = 'Temperature in san fransisco';
 
 const svg = d3.select('svg');
@@ -11,7 +10,7 @@ const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 
 
-d3.csv('temperature.csv').then(data => {
+d3.csv('data-canvas.csv').then(data => {
     data.forEach(d => {
         d.temperature = +d.temperature;
         d.timestamp = new Date(d.timestamp);
@@ -25,23 +24,31 @@ d3.csv('temperature.csv').then(data => {
     const yValue = d => d.temperature;
     const yAxisLabel = 'Temperature';
 
+    const colorValue = d => d.city;
+
+    // value range for x axis
     const xScale = d3.scaleTime()
         .domain(d3.extent(data, xValue))
         .range([0, innerWidth])
         .nice();
 
+    // value range for y axis
     const yScale = d3.scaleLinear()
         .domain(d3.extent(data, yValue))
         .range([innerHeight, 0])
         .nice();
 
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+
     const g = svg.append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
+    // grid line for height
     const xAxis = d3.axisBottom(xScale)
         .tickSize(-innerHeight)
         .tickPadding(15);
 
+    // grid line for width
     const yAxis = d3.axisLeft(yScale)
         .tickSize(-innerWidth)
         .tickPadding(10);
@@ -76,12 +83,25 @@ d3.csv('temperature.csv').then(data => {
         .x(d => xScale(xValue(d)))
         .y(d => yScale(yValue(d)));
 
-    g.append('path')
-        .attr('class', 'line-path')
-        .attr('d', lineGenerator(data))
+    const nested = d3.group(data, d => d.city);
 
-    g.append('text')
+    var domain = [];
+    nested.forEach(d => {
+        domain.push(d[0].city);
+    });
+    colorScale.domain(domain);
+
+    g.selectAll('.line-path')
+        .data(nested)
+        .enter()
+        .append('path')
+        .attr('class', 'line-path')
+        .attr('d', d => lineGenerator(d[1]))
+        .attr('stroke', d => colorScale(d[0]));
+
+    svg.append('text')
         .attr('class', 'title')
-        .attr('y', -10)
+        .attr('x', width / 2)
+        .attr('y', 45)
         .text(titleText);
 });
